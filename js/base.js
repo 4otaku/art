d44b87d975c8a0bfb16aa3f39373bd3698cac396
Nameless <admin@4otaku.ru>
2012-04-02 21:43:28 +0400

    workan

--- js/base.js
+++ js/base.js
@@ -1,3 +1,5 @@
+// Object adding functions
+
 function init_objects(data) {
 	$.each(data, function(type, objects) {
 		$.each(objects, function(dev_null, object) {
@@ -41,56 +43,56 @@ function add_event(events, type, section, fn) {
 
 // Base object
 
-var OBJECT = {}
+var OBJECT = {
+	base: function (id, events, values) {
+		events = events || {};
+		values = values || {};
 
-OBJECT.base = function (id, events, values) {
-	events = events || {};
-	values = values || {};
+		// default empty values
+		this.child = this.child || {};
+		this.class_name = this.class_name || '';
+		this.id = id || '';
 
-	// default empty values
-	this.child = this.child || {};
-	this.class_name = this.class_name || '';
-	this.id = id || '';
+		if (this.class_name && this.id) {
+			this.el = $('#' + this.class_name + '_' + this.id).first();
 
-	if (this.class_name && this.id) {
-		this.el = $('#' + this.class_name + '_' + this.id).first();
+			$.each(this.child, $.proxy(function(key, value) {
+				this.child[key] = this.el.find(value);
+			}, this));
+		} else {
+			this.el = false;
+			this.child = {};
+		}
 
-		$.each(this.child, $.proxy(function(key, value) {
-			this.child[key] = this.el.find(value);
+		$.each(values, $.proxy(function(key, value) {
+			this[key] = value;
 		}, this));
-	} else {
-		this.el = false;
-		this.child = {};
-	}
 
-	$.each(values, $.proxy(function(key, value) {
-		this[key] = value;
-	}, this));
+		$.each(events, $.proxy(function(key, event) {
+			if (this.child[key]) {
+				$.each(event, $.proxy(function(key2, event2) {
+					if (!$.isArray(event2)) {
+						event2 = [event2];
+					}
 
-	$.each(events, $.proxy(function(key, event) {
-		if (this.child[key]) {
-			$.each(event, $.proxy(function(key2, event2) {
-				if (!$.isArray(event2)) {
-					event2 = [event2];
+					$.each(event2, $.proxy(function(dev_null, bind) {
+						this.child[key].on(key2, $.proxy(function(e){
+							bind.call(this, e);
+						}));
+					}, this));
+				}, this));
+			} else {
+				if (!$.isArray(event)) {
+					event = [event];
 				}
-
-				$.each(event2, $.proxy(function(dev_null, bind) {
-					this.child[key].on(key2, $.proxy(function(e){
+				$.each(event, $.proxy(function(dev_null, bind) {
+					this.el.on(key, $.proxy(function(e){
 						bind.call(this, e);
 					}));
 				}, this));
-			}, this));
-		} else {
-			if (!$.isArray(event)) {
-				event = [event];
 			}
-			$.each(event, $.proxy(function(dev_null, bind) {
-				this.el.on(key, $.proxy(function(e){
-					bind.call(this, e);
-				}));
-			}, this));
-		}
-	}, this));
+		}, this));
+	}
 }
 
 // Common objects
@@ -140,7 +142,7 @@ OBJECT.settings = function(id, events, values) {
 	}
 }
 
-OBJECT.form = function(id, events, values) {
+OBJECT.form = function(id, values, events) {
 
 	this.class_name = 'form';
 
@@ -161,9 +163,28 @@ OBJECT.form = function(id, events, values) {
 				data[this.name] = $(this).val();
 			}
 		});
-		this.disabled = true;
+		this.child.error.hide();
+		this.child.submit.hide();
 		this.child.loader.show();
-		Ajax.perform(this.url, this.mask_el, this.success)
+		Ajax.perform(this.url, data, $.proxy(function(response) {
+			this.child.submit.show();
+			this.child.loader.hide();
+			if (response.success == false) {
+				var message = '';
+				$.each(response.errors, function(dev_null, error) {
+					error = Ajax.translate_error(error);
+					if (error) {
+						message += error + '<br />';
+					}
+				});
+				this.child.error.html(message).show();
+			} else {
+				this.success.call(this, data);
+			}
+		}, this), $.proxy(function(response) {
+			this.child.submit.show();
+			this.child.loader.hide();
+		}, this));
 	}
 
 	events = add_event(events, 'on_enter', 'keydown', $.proxy(function(e) {
@@ -174,4 +195,35 @@ OBJECT.form = function(id, events, values) {
 	events = add_event(events, 'submit', 'click', $.proxy(this.submit, this));
 
 	OBJECT.base.call(this, id, events, values);
+}
+
+// Ajax functions
+
+var Ajax = {
+	perform: function(url, data, success, failure) {
+		$.ajax({
+			url: url,
+			data: data ? data: {},
+			type: 'POST',
+			success: success ? success : false,
+			failure: failure ? failure : false,
+			dataType: 'json'
+		});
+	},
+
+	error: {
+		410: 'Указан неправильный адрес в запросе.'
+	},
+
+	translate_error: function(error) {
+		if (this.error[error.code]) {
+			return this.error[error.code];
+		}
+
+		if (error.message) {
+			return error.message;
+		}
+
+		return 'Код ошибки: ' + error.code;
+	}
 }
