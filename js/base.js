0628a4cf0ac69abf56ff1494be1625e6b047d126
Nameless <admin@4otaku.ru>
2012-04-03 20:43:46 +0400

    new_javascript_inheritance

--- js/base.js
+++ js/base.js
@@ -1,3 +1,34 @@
+// Base OOP functions
+
+function mixin(dst, src) {
+	var tobj = {}
+	for(var x in src) {
+		if((typeof tobj[x] == "undefined") || (tobj[x] != src[x])) {
+			dst[x] = src[x];
+		}
+	}
+	if(document.all && !document.isOpera) {
+		var p = src.toString;
+		if(typeof p == "function" && p != dst.toString && p != tobj.toString &&
+			p != "\nfunction toString() {\n    [native code]\n}\n") {
+
+			dst.toString = src.toString;
+		}
+	}
+}
+
+function extend(Child, Parent, mixinData) {
+	var F = function() {};
+	F.prototype = Parent.prototype;
+	Child.prototype = new F();
+	Child.prototype.constructor = Child;
+	Child.superclass = Parent.prototype;
+
+	if (typeof mixinData == 'object') {
+		mixin(Child.prototype, mixinData);
+	}
+}
+
 // Object adding functions
 
 function init_objects(data) {
@@ -5,18 +36,18 @@ function init_objects(data) {
 		$.each(objects, function(dev_null, object) {
 			if (OBJECT[type]) {
 				var id = object[0];
-				var events = object[1] || {};
-				var values = object[2] || {};
-				new OBJECT[type](id, events, values);
+				var values = object[1] || {};
+				var events = object[2] || {};
+				new OBJECT[type](id, values, events);
 			}
 		});
 	});
 }
 
-function init(type, id, events, values) {
+function init(type, id, values, events) {
 	events = events || {};
 	values = values || {};
-	new OBJECT[type](id, events, values);
+	new OBJECT[type](id, values, events);
 }
 
 function add_event(events, type, section, fn) {
@@ -44,17 +75,56 @@ function add_event(events, type, section, fn) {
 // Base object
 
 var OBJECT = {
-	base: function (id, events, values) {
+	base: function (id, values, events) {
 		events = events || {};
 		values = values || {};
+		events = this.extend_events(events, this.events);
+		values = $.extend(values, this.values, true);
 
-		// default empty values
-		this.child = this.child || {};
-		this.class_name = this.class_name || '';
-		this.id = id || '';
+		this.init_elements(id);
+		this.init_values(values);
+		this.init_events(events);
+	}
+}
 
-		if (this.class_name && this.id) {
-			this.el = $('#' + this.class_name + '_' + this.id).first();
+mixin(OBJECT.base.prototype, {
+	child: {},
+	class_name: 'base',
+	id: '',
+	events: {},
+	values: {},
+
+	extend_events: function(from, to) {
+		$.each(to, $.proxy(function(key, value) {
+			if (from[key] == undefined) {
+				from[key] = to[key];
+			} else {
+				if (!$.isArray(value) && typeof value == 'object') {
+					$.each(to[key], $.proxy(function(key2, value2) {
+						if (from[key][key2] == undefined) {
+							from[key][key2] = value2;
+						} else {
+							if (!$.isArray(from[key][key2])) {
+								from[key][key2] = [from[key][key2]];
+							}
+							from[key][key2].push(value2);
+						}
+					}, this));
+				} else {
+					if (!$.isArray(from[key])) {
+						from[key] = [from[key]];
+					}
+					from[key].push(value);
+				}
+			}
+		}, this));
+
+		return from;
+	},
+
+	init_elements: function(id) {
+		if (this.class_name && id) {
+			this.el = $('#' + this.class_name + '_' + id).first();
 
 			$.each(this.child, $.proxy(function(key, value) {
 				this.child[key] = this.el.find(value);
@@ -63,11 +133,15 @@ var OBJECT = {
 			this.el = false;
 			this.child = {};
 		}
+	},
 
+	init_values: function(values) {
 		$.each(values, $.proxy(function(key, value) {
 			this[key] = value;
 		}, this));
+	},
 
+	init_events: function(events) {
 		$.each(events, $.proxy(function(key, event) {
 			if (this.child[key]) {
 				$.each(event, $.proxy(function(key2, event2) {
@@ -78,7 +152,7 @@ var OBJECT = {
 					$.each(event2, $.proxy(function(dev_null, bind) {
 						this.child[key].on(key2, $.proxy(function(e){
 							bind.call(this, e);
-						}));
+						}, this));
 					}, this));
 				}, this));
 			} else {
@@ -88,48 +162,38 @@ var OBJECT = {
 				$.each(event, $.proxy(function(dev_null, bind) {
 					this.el.on(key, $.proxy(function(e){
 						bind.call(this, e);
-					}));
+					}, this));
 				}, this));
 			}
 		}, this));
 	}
-}
+});
+
 
 // Common objects
 
 OBJECT.clickable = function(id, values) {
-
-	this.class_name = 'clickable';
-
-	events = {
-		'click': $.proxy(function() {
-			if (this.func) {
-				this.func.call(this);
-			}
-		}, this)
-	};
-
 	if (typeof values == 'function') {
 		values = {func: values};
 	}
 
-	OBJECT.base.call(this, id, events, values);
+	OBJECT.clickable.superclass.constructor.call(this, id, values);
 }
 
-OBJECT.settings = function(id, events, values) {
+extend(OBJECT.clickable, OBJECT.base, {
+	class_name: 'clickable',
+	events: {
+		click: function() {
+			if (this.func) {
+				this.func.call(this);
+			}
+		}
+	}
+});
 
-	this.class_name = 'settings';
+OBJECT.settings = function(id, values, events) {
 
-	events = add_event(events, 'change', $.proxy(function() {
-		var value = this.el.is(':checkbox') ?
-			(this.el.attr("checked") == 'checked') - 0 :
-			this.el.val();
-
-		$.get('/ajax/set?section=' + this.section + '&key=' +
-			this.key + '&value=' + value);
-	}, this));
-
-	OBJECT.base.call(this, id, events, values);
+	OBJECT.settings.superclass.constructor.call(this, id, values, events);
 
 	if (this.el.is(':checkbox')) {
 		if (this.value == 0) {
@@ -142,21 +206,68 @@ OBJECT.settings = function(id, events, values) {
 	}
 }
 
+extend(OBJECT.settings, OBJECT.base, {
+	class_name: 'settings',
+	events: {
+		change: function() {
+			var value = this.el.is(':checkbox') ?
+				(this.el.attr("checked") == 'checked') - 0 :
+				this.el.val();
+
+			$.get('/ajax/set?section=' + this.section + '&key=' +
+				this.key + '&value=' + value);
+		}
+	}
+});
+
 OBJECT.form = function(id, values, events) {
 
-	this.class_name = 'form';
+	values.validate = values.validate ? values.validate : {};
 
-	this.child = {
+	OBJECT.base.call(this, id, values, events);
+}
+
+extend(OBJECT.form, OBJECT.base, {
+	class_name: 'form',
+	child: {
 		on_enter: 'input[type=text],input[type=password]',
 		submit: '.submit',
 		loader: 'div.loader',
 		error: 'div.error'
-	};
+	},
+	submit: function() {
+		var data = this.get_data();
 
-	this.submit = function() {
-		if (this.disabled) {
+		if (data.error.length) {
+			this.child.error.html(data.error.join('<br />')).show();
 			return;
 		}
+
+		this.child.error.hide();
+		this.child.submit.hide();
+		this.child.loader.show();
+
+		Ajax.perform(this.url, data.data, $.proxy(function(response) {
+			this.child.submit.show();
+			this.child.loader.hide();
+			if (response.success == false) {
+				var message = '';
+				$.each(response.errors, function(dev_null, error) {
+					error = Ajax.translate_error(error);
+					if (error) {
+						message += error + '<br />';
+					}
+				});
+				this.child.error.html(message).show();
+			} else {
+				this.success.call(this, response);
+			}
+		}, this), $.proxy(function(response) {
+			this.child.submit.show();
+			this.child.loader.hide();
+		}, this));
+	},
+	get_data: function() {
 		var data = {};
 		var error = [];
 		var me = this;
@@ -181,47 +292,26 @@ OBJECT.form = function(id, values, events) {
 				data[this.name] = val;
 			}
 		});
-
-		if (error.length) {
-			this.child.error.html(error.join('<br />')).show();
-			return;
+		return {
+			error: error,
+			data: data
 		}
-
-		this.child.error.hide();
-		this.child.submit.hide();
-		this.child.loader.show();
-		Ajax.perform(this.url, data, $.proxy(function(response) {
-			this.child.submit.show();
-			this.child.loader.hide();
-			if (response.success == false) {
-				var message = '';
-				$.each(response.errors, function(dev_null, error) {
-					error = Ajax.translate_error(error);
-					if (error) {
-						message += error + '<br />';
-					}
-				});
-				this.child.error.html(message).show();
-			} else {
-				this.success.call(this, data);
+	},
+	events: {
+		on_enter: {
+			keydown: function(e) {
+				if (e.which == 13) {
+					this.submit();
+				}
 			}
-		}, this), $.proxy(function(response) {
-			this.child.submit.show();
-			this.child.loader.hide();
-		}, this));
-	}
-
-	events = add_event(events, 'on_enter', 'keydown', $.proxy(function(e) {
-		if (e.which == 13) {
-			this.submit();
+		},
+		submit: {
+			click: function() {
+				this.submit();
+			}
 		}
-	}, this));
-	events = add_event(events, 'submit', 'click', $.proxy(this.submit, this));
-
-	values.validate = values.validate ? values.validate : {};
-
-	OBJECT.base.call(this, id, events, values);
-}
+	}
+});
 
 // Ajax functions
 
