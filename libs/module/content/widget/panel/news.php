2c80ac0bbacdd61a39b25cb20b495dc4f474db64
Nameless <admin@4otaku.ru>
2012-04-13 23:47:59 +0400

    wiki_panel

--- libs/module/content/widget/panel/news.php
+++ libs/module/content/widget/panel/news.php
@@ -3,6 +3,9 @@
 class Module_Content_Widget_Panel_News extends Module_Content_Widget_Panel_Abstract
 {
 	protected function make_request() {
-		return new Request_Single('news', $this, array('area' => 'main'));
+		return new Request_Single('news', $this, array(
+			'area' => 'main',
+			'fields' => array('title', 'comment_count')
+		));
 	}
 }
--- libs/module/content/widget/panel/wiki.php
+++ libs/module/content/widget/panel/wiki.php
@@ -2,4 +2,43 @@
 
 class Module_Content_Widget_Panel_Wiki extends Module_Content_Widget_Panel_Abstract
 {
+	private $wiki_namespaces = array(
+		1 => 'Обсуждение',
+		2 => 'Участник',
+		3 => 'Обсуждение_участника',
+		4 => 'wiki',
+		5 => 'Обсуждение_wiki',
+		6 => 'Файл',
+		7 => 'Обсуждение_файла',
+		8 => 'MediaWiki',
+		9 => 'Обсуждение_MediaWiki',
+		10 => 'Шаблон',
+		11 => 'Обсуждение_шаблона',
+		12 => 'Справка',
+		13 => 'Обсуждение_справки',
+		14 => 'Категория',
+		15 => 'Обсуждение_категории',
+		500 => 'Тег',
+		501 => 'Обсуждение_тега',
+	);
+
+	protected function make_request() {
+		return new Request_Single('recentchanges', $this, array(
+			'fields' => array('rc_title', 'rc_namespace'),
+			'mode' => 'no_count'));
+	}
+
+	public function recieve_data($data) {
+		$data = $data['data'];
+		if (array_key_exists($data['rc_namespace'], $this->wiki_namespaces)) {
+			$name = $this->wiki_namespaces[$data['rc_namespace']].':'.$data['rc_title'];
+		} else {
+			$name = $data['rc_title'];
+		}
+
+		$url = str_replace(array('%3A', '%2F'), array(':', '/'), urlencode($name));
+
+		$this->set_param('name', $name);
+		$this->set_param('url', $url);
+	}
 }
