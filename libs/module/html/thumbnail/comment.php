c579eba5a3cfd891df2fec5fff8a10c9a1bf0c6f
Nameless <admin@4otaku.ru>
2013-06-03 08:49:35 +0400

    fixes

--- libs/module/html/thumbnail/comment.php
+++ libs/module/html/thumbnail/comment.php
@@ -2,6 +2,8 @@
 
 class Module_Html_Thumbnail_Comment extends Module_Html_Thumbnail_Abstract
 {
+	protected $css = array('comment');
+
 	protected function make_tooltip($data) {
 		$username = $data['comment']['username'];
 		$text = new Text($data['comment']['text']);
