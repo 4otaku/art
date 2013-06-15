d3ec9b368ddaaf6fe1cde8bbe44b7e1184174871
shtrih <shtrih@hitagi.ru>
2013-06-15 15:52:24 +0900

    Fix [collapsible_sidebar] убрал css из js

--- js/plugins/collapsible_sidebar.head.js
+++ js/plugins/collapsible_sidebar.head.js
@@ -1,16 +1,3 @@
 if (localStorage.getItem('art-sidebar-collapsed')) {
-	var css = '.sidebar {width: 40px;} ' +
-			'.sidebar_part {display: none;}' +
-			'.art, .images {margin-left: 45px;}',
-		head = document.getElementsByTagName('head')[0],
-		style = document.createElement('style');
-
-	style.type = 'text/css';
-	if (style.styleSheet){
-		style.styleSheet.cssText = css;
-	} else {
-		style.appendChild(document.createTextNode(css));
-	}
-
-	head.appendChild(style);
+	document.getElementsByTagName('head')[0].setAttribute('class', 'collapsible_sidebar')
 }
