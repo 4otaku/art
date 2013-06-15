3cafe5b0d80159fba04291998c5190bfac64317a
Nameless <admin@4otaku.ru>
2013-06-15 09:35:48 +0400

    collapsible starting state

--- js/plugins/collapsible_sidebar.head.js
+++ js/plugins/collapsible_sidebar.head.js
@@ -1,3 +1,16 @@
 if (localStorage.getItem('art-sidebar-collapsed')) {
-	$('.sidebar').css('width', '40px');
+	var css = '.sidebar {width: 40px;} ' +
+			'.sidebar_part {display: none;}' +
+			'.art, .images {margin-left: 45px;}',
+		head = document.getElementsByTagName('head')[0],
+		style = document.createElement('style');
+
+	style.type = 'text/css';
+	if (style.styleSheet){
+		style.styleSheet.cssText = css;
+	} else {
+		style.appendChild(document.createTextNode(css));
+	}
+
+	head.appendChild(style);
 }
